ad8c80b7bbfe14835cd8efda24f69618dee1a6d0
Samuel <samuelgio14@gmail.com>
2024-04-26 17:07:54 +0200

    Aggiunto PBT1

--- src/test/java/FractionPropertyBasedTest.java
+++ src/test/java/FractionPropertyBasedTest.java
@@ -1,30 +1,26 @@
 import it.uniba.itss2324.SAFR.homeworks.Fraction;
-import net.jqwik.api.Arbitraries;
-import net.jqwik.api.Arbitrary;
-import net.jqwik.api.Assume;
-import net.jqwik.api.Combinators;
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
-import net.jqwik.api.Provide;
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+import net.jqwik.api.statistics.Histogram;
+import net.jqwik.api.statistics.Statistics;
+import net.jqwik.api.statistics.StatisticsReport;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FractionPropertyBasedTest 
 {
-        @Property
-        void validFraction(@ForAll ("validFractionRange") Fraction fraction)
-        {
-            Assume.that(fraction.getDenominator() != 0 && fraction.getDenominator() != Integer.MIN_VALUE && fraction.getNumerator() != Integer.MIN_VALUE);
+    // PBT1
+    @Property
+    @Report(Reporting.GENERATED)
+    @StatisticsReport(format = Histogram.class)
+    void reducedFractionCannotHaveZeroDenominator(@ForAll @IntRange(min = Integer.MIN_VALUE + 1) int numerator,
+                                                  @ForAll @IntRange(min = Integer.MIN_VALUE + 1) int denominator)
+    {
+        Assume.that(denominator != 0);
 
-            Fraction f1 = new Fraction(fraction.getNumerator(), fraction.getDenominator());
-            assertEquals(f1, fraction);
-        }
+        Fraction reducedFraction = Fraction.getReducedFraction(numerator, denominator);
 
-        @Provide
-        private Arbitrary<Fraction> validFractionRange()
-        {
-            Arbitrary<Integer> numerator = Arbitraries.integers().between(Integer.MIN_VALUE, Integer.MAX_VALUE);
-            Arbitrary<Integer> denominator = Arbitraries.integers().between(Integer.MIN_VALUE, Integer.MAX_VALUE);
-            return Combinators.combine(numerator, denominator).as(Fraction::new);
-        }
+        assertTrue(reducedFraction.getDenominator() != 0);
+    //  Statistics.collect();
+    }
 }
